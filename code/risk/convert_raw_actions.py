bbfb7a2c8af4f8af83d808f887c2200b65810f81
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-12-09 22:28:21 -0600

    Update convert_raw_actions.py

--- code/risk/convert_raw_actions.py
+++ code/risk/convert_raw_actions.py
@@ -1,21 +1,26 @@
 import pandas as pd
 
-# Load the raw data
-raw_data = pd.read_csv('provider_actions_raw.csv')
+# File locations
+provider_dir = "/mnt/batch/tasks/shared/LS_root/mounts/clusters/pod-mlcomputeinstance2/code/Data/providers/"
+feature_dir = "/mnt/batch/tasks/shared/LS_root/mounts/clusters/pod-mlcomputeinstance2/code/Data/features/"
+labels_dir = "/mnt/batch/tasks/shared/LS_root/mounts/clusters/pod-mlcomputeinstance2/code/Data/labels/"
 
-# Load additional data
-comments_data = pd.read_csv('features/features_comments.csv')
-pass_data = pd.read_csv('features_pass.csv')
-recent_case_data = pd.read_csv('features/features_recent_case.csv')
+# Load the raw data
+raw_data = pd.read_csv(provider_dir + 'provider_actions_raw.csv')
+
+# Load additional data 
+comments_data = pd.read_csv(feature_dir +'features_comments.csv')
+pass_data = pd.read_csv(feature_dir + 'features_pass.csv')
+recent_case_data = pd.read_csv(feature_dir + 'features_recent_case.csv')
 
 # Merge the raw data with comments scores
-data_with_comments = pd.merge(raw_data, comments_data[['Provider ID', 'Comments Score']], on='Provider ID')
+data_with_comments = pd.merge(raw_data, comments_data[['Comments', 'Comment Score']], on='Comments')
 
 # Merge with pass scores
-data_with_pass = pd.merge(data_with_comments, pass_data[['Provider ID', 'Reason for Pass Score']], on='Provider ID')
+data_with_pass = pd.merge(data_with_comments, pass_data[['Reason for pass', 'Pass Score']], on='Pass Score')
 
 # Merge with recent case scores
-final_data = pd.merge(data_with_pass, recent_case_data[['Provider ID', 'OpenDtScore', 'CloseDtScore', 'DataMiningUpdateDtScore']], on='Provider ID')
+final_data = pd.merge(data_with_pass, recent_case_data[['Year', 'OpenDtScore', 'CloseDtScore', 'DataMiningUpdateDtScore']], on='Year')
 
 # Rename columns to match your final dataset
 final_data.rename(columns={
@@ -25,6 +30,6 @@ final_data.rename(columns={
 }, inplace=True)
 
 # Save the merged data
-final_data.to_csv('provider_actions.csv', index=False)
+final_data.to_csv(labels_dir + 'provider_actions.csv', index=False)
 
 print("Data processing complete. The merged data is saved in 'provider_actions.csv'.")
